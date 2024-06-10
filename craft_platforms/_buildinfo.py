87a3b33dbc658fce14557c3aaf3672253c94d08c
Alex Lowe <alex.lowe@canonical.com>
2024-06-10 11:35:34 -0400

    fix: make buildinfo use the correct type

--- craft_platforms/_buildinfo.py
+++ craft_platforms/_buildinfo.py
@@ -31,5 +31,5 @@ class BuildInfo:
     build_for: _architectures.DebianArchitecture
     """The architecture to build for."""
 
-    build_base: _distro.BaseName
+    build_base: _distro.DistroBase
     """The base to build on."""
