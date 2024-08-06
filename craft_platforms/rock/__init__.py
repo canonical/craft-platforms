566253b37b6f38725135e606943ec90149900067
Callahan Kovacs <callahankovacs@gmail.com>
2024-08-06 07:46:01 -0500

    style: typo
    Signed-off-by: Callahan Kovacs <callahankovacs@gmail.com>

--- craft_platforms/rock/__init__.py
+++ craft_platforms/rock/__init__.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""Roc-specific build information for craft-platforms."""
+"""Rock-specific build information for craft-platforms."""
 
 from ._build import RockBuildInfo, get_platforms_rock_build_plan
 
