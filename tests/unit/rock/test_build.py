80ac005910cca6cc3ccc946f720c2917b25376ca
Imani Pelton <imani.pelton@canonical.com>
2025-04-10 13:03:13 -0400

    fix: validate bare bases for rockcraft (#123)

--- tests/unit/rock/test_build.py
+++ tests/unit/rock/test_build.py
@@ -20,7 +20,7 @@ import itertools
 import craft_platforms
 import pytest
 import pytest_check
-from craft_platforms import rock
+from craft_platforms import _errors, rock
 
 SAMPLE_UBUNTU_VERSIONS = ("16.04", "18.04", "20.04", "22.04", "24.04", "24.10", "devel")
 
@@ -264,3 +264,9 @@ def test_build_plans_bad_base(base, error_msg):
 def test_build_plans_bad_architecture(platforms, error_msg):
     with pytest.raises(ValueError, match=error_msg):
         rock.get_rock_build_plan("ubuntu@24.04", platforms)
+
+
+def test_bare_base_no_build_base() -> None:
+    """Make sure that an error is raised if base=="bare" but build-base==None"""
+    with pytest.raises(_errors.NeedBuildBaseError):
+        rock.get_rock_build_plan("bare", platforms={"amd64": None}, build_base=None)
